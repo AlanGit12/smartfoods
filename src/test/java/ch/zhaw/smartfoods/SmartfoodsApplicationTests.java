f704a750905519088a80e5768b7bf395b4558ec8
AlanGit12 <gradeala@students.zhaw.ch>
2026-03-21 11:20:17 +0100

    Projekt gestartet

---
+++ src/test/java/ch/zhaw/smartfoods/SmartfoodsApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.zhaw.smartfoods;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartfoodsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
