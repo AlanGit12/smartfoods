83b280bf04126ba87510c89094e3606ee93878f5
AlanGit12 <gradeala@students.zhaw.ch>
2026-03-21 14:07:01 +0100

    InventoryItemController hinzugefügt. Post, GetAll und GetById endpoints hinzugefügt. Closes #4

---
+++ src/main/java/ch/zhaw/smartfoods/controller/InventoryItemController.java
@@ -0,0 +1,59 @@
+package ch.zhaw.smartfoods.controller;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import ch.zhaw.smartfoods.model.InventoryItem;
+import ch.zhaw.smartfoods.model.InventoryItemCreateDTO;
+import ch.zhaw.smartfoods.repository.InventoryItemRepository;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+@RequestMapping("/api")
+public class InventoryItemController {
+    
+
+    @Autowired
+    InventoryItemRepository inventoryItemRepository;
+
+    @PostMapping("/inventory")
+    public ResponseEntity<InventoryItem> createItem (@RequestBody InventoryItemCreateDTO item) {
+
+     InventoryItem newItem = new InventoryItem(
+        item.getName(),
+        item.getQuantity(),
+        item.getExpiryDate(),
+        item.getPrice()
+    );
+    newItem.setPurchaseDate(LocalDate.now());
+     InventoryItem saved = inventoryItemRepository.save(newItem);
+        return new ResponseEntity<>(saved, HttpStatus.CREATED);
+    }
+    
+    @GetMapping("/inventory")
+    public ResponseEntity <List<InventoryItem>> getAllItems (){
+        return ResponseEntity.ok(inventoryItemRepository.findAll());
+    }
+
+
+     @GetMapping("/inventory/{id}")
+    public ResponseEntity<InventoryItem> getItemById(@PathVariable String id) {
+        Optional<InventoryItem> item = inventoryItemRepository.findById(id);
+        if (item.isPresent()) {
+            return ResponseEntity.ok(item.get());
+        }
+        return ResponseEntity.notFound().build();
+    }
+}
