2c493d8622f861cf335c7af8a0eca7cf9d81c0b4
AlanGit12 <gradeala@students.zhaw.ch>
2026-04-02 00:13:51 +0200

    Model ItemTemplate und CreateDTO hinzugefügt. Closes #16

---
+++ src/main/java/ch/zhaw/smartfoods/model/ItemTemplate/ItemTemplate.java
@@ -0,0 +1,30 @@
+package ch.zhaw.smartfoods.model.ItemTemplate;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import ch.zhaw.smartfoods.enums.ProductUnit;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Getter
+@Setter
+@Document("ItemTemplate")
+public class ItemTemplate {
+    
+    @Id
+    private String id;
+    @NonNull
+    private String name;
+    @NonNull
+    private ProductUnit defaultUnit;
+    @NonNull
+    private Double defaultPrice;
+    @NonNull
+    private Double defaultAmount;
+}
---
+++ src/main/java/ch/zhaw/smartfoods/model/ItemTemplate/ItemTemplateCreateDTO.java
@@ -0,0 +1,17 @@
+package ch.zhaw.smartfoods.model.ItemTemplate;
+
+import ch.zhaw.smartfoods.enums.ProductUnit;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class ItemTemplateCreateDTO {
+    
+    private String name;
+    private ProductUnit defaultUnit;
+    private Double defaultPrice;
+    private Double defaultAmount;
+}
